05ed844073862de2c9c4dc695aa7acdc08cbc53f
asif ali <asifali0369@outlook.com>
2023-01-14 14:49:15 +0530

    second day assignment

---
+++ logical-and-ternary-operator/problem2.js
@@ -0,0 +1,8 @@
+let char = "a";
+
+if ((char== "a") || (char== "e") || (char== "i") || (char== "o") || (char== "u")){
+  console.log("vowel");
+}
+else {
+  console.log("not a vowel");
+}
---
+++ logical-and-ternary-operator/problem4.js
@@ -0,0 +1,13 @@
+let a = 1;
+let b = 2;
+let c = 3;
+
+if (a>b && a>c){
+  console.log("a is greater");
+}
+else if (c>a && c>b){
+  console.log("c is greater");
+}
+else {
+  console.log("b is greater");
+}
